3a633a8cb79451fd27815c868d800139cdc5a9b1
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-02-19 12:42:59 +0100

    refactor: navbar generated in script, added active class and mockups for other pages

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+// Navbar
+
+const NAVBAR_CONTENT = `
+<h1 id="title" class="animated-type"></h1>
+<div class="menu">
+  <ul>
+    <li><a href="/">Noticias</a></li>
+    <li><a href="events.html">Eventos</a></li>
+    <li><a href="repos.html">Repositorios</a></li>
+    <li><a href="resources.html">Recursos</a></li>
+  </ul>
+</div>
+`;
+
+const navbar = document.getElementById("navbar");
+navbar.innerHTML = NAVBAR_CONTENT;
+
+// Set active link
+
+const links = document.querySelectorAll("#navbar .menu ul li a");
+
+for (let i = 0; i < links.length; i++) {
+  if (links[i].href === window.location.href) {
+    links[i].classList.add("active");
+  }
+}
+
 const titles = [
   "Alicante Open Source",
   "alicanteOpenSource",
